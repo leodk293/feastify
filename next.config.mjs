a0503328c143fb64a5dcb1e6c7bef6a619952446
Aboubacar Traore <aboubatrao04@gmail.com>
2024-11-26 17:51:14 +0100

    images

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['www.themealdb.com','lh3.googleusercontent.com'],
+        remotePatterns:[
+            {
+                protocol: "https",
+                hostname: "**",
+            }
+        ]
     },
 };
 export default nextConfig;
