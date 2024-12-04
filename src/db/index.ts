e24b08db25c8ffe45764cef0c7adb53f4774dd1c
Aboubacar Traore <aboubatrao04@gmail.com>
2024-12-04 19:59:26 +0100

    code updated

--- src/db/index.ts
+++ src/db/index.ts
@@ -4,9 +4,6 @@ import { createClient } from "@libsql/client";
 
 config({ path: ".env.local" });
 
-/*console.log("TURSO_CONNECTION_URL:", process.env.TURSO_CONNECTION_URL);
-console.log("TURSO_AUTH_TOKEN:", process.env.TURSO_AUTH_TOKEN);*/
-
 const client = createClient({
   url: process.env.NEXT_PUBLIC_TURSO_CONNECTION_URL!,
   authToken: process.env.NEXT_PUBLIC_TURSO_AUTH_TOKEN!,
